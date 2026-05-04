ba4ec8c9a5100af5c67d65c4ff107a839fef5725
manjulamp599-eng <manjulamp599@gmail.com>
2026-05-04 12:04:02 +0530

    Implement bitwise swapping of two numbers

---
+++ swappingtwonumbersbitwise.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        
+        
+        System.out.println("a="+a+",b="+b);
+    }
+}
