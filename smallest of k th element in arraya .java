9948fc35e5b49346af078d755383051302b92408
manjulamp599-eng <manjulamp599@gmail.com>
2026-05-05 10:29:46 +0530

    Implement finding the k-th smallest element in array

---
+++ smallest of k th element in arraya .java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int [] arr ={7,10,4,3,20,15};
+        int k = 3;
+        Arrays.sort (arr);
+        int kthSmallest=arr[k-1];
+        System.out.println("kth smallest element is:"+ kthSmallest);
+         
+    }
+}
