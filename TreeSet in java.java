2fa8194de1985dfa26cf9793872fb90ff6fb012a
manjulamp599-eng <manjulamp599@gmail.com>
2026-05-06 11:51:21 +0530

    Add TreeSet example with add, remove, and contains methods
    Implement a TreeSet example in Java with basic operations.

---
+++ TreeSet in java.java
@@ -0,0 +1,18 @@
+import java.util.TreeSet;
+class Main {
+    public static void main(String[] args) {
+        TreeSet<Integer>cse=new TreeSet<>();
+        cse.add(30);
+        cse.add(3);
+        cse.add(12);
+        cse.add(13);
+        cse.add(22);
+        cse.add(23);
+        cse.remove(30);
+        cse.size();
+        cse.add(31);
+    System.out.println(cse);
+    System.out.println(cse.contains(31));
+    System.out.println("cse="+cse);
+    }
+}
