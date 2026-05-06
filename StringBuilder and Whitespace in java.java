fd171a5a3dfdf64c2e8ce1582996e769a308ca23
manjulamp599-eng <manjulamp599@gmail.com>
2026-05-06 15:46:48 +0530

    Add StringBuilder example to remove whitespace

---
+++ StringBuilder and Whitespace in java.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        String input = "Hello World Java";
+        StringBuilder k= new StringBuilder();
+        for (char j: input.toCharArray()){
+           if(! Character.isWhitespace(j)){
+           k.append(j);
+           }
+        }
+           String m=k.toString();
+        System.out.println(m);
+    }
+}
