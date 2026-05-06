330e2f8d6d28d164e77acea1e972a4eee5cf3b5b
manjulamp599-eng <manjulamp599@gmail.com>
2026-05-06 09:31:44 +0530

    Add Reverse String implementation in Java

---
+++ Reverse String.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        String s ="hello";
+        String rev = "";
+          for (int i=0; i<s.length();i++){
+              rev = s.charAt(i)+rev ;
+              
+          }
+        System.out.println(rev);
+    }
+}
