a8d1447b572d1df9819abe3f3b8cb9355e34f6e7
manjulamp599-eng <manjulamp599@gmail.com>
2026-05-06 14:37:12 +0530

    Remove spaces from a string in Main class

---
+++ Remove space from String .java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        
+        
+        String result = str.replace(" ", "");
+        
+        System.out.println(result);
+    }
+}
